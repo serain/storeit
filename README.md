56b2b48b6ddddbc07cb3f13336fcbc924868ea0a
Alexandre Kaskasoli <alexandre.kaskasoli@mwrinfosecurity.com>
2018-12-17 13:33:08 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# StoreIt
+
+> Small application that interacts with a CosmosDB to read/write data.
+
+This is part of an Azure/Terraform/Ansible workshop.
+
+## Setup
+
+```
+git clone https://github.com/serain/storeit
+pip3 install -r requirements.txt
+export STOREIT_MONGO_URI="mongodb://mongo:27017/"
+python3 app.py
+```
---
+++ app.py
@@ -0,0 +1,39 @@
+import os
+from flask import Flask, render_template_string, request
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+app.config['MONGO_URI'] = os.environ['STOREIT_MONGO_URI']
+mongo = PyMongo(app)
+
+HTML = '''
+<html>
+<head><title>StoreIt</title></head>
+<body>
+    <h2>StoreIt</h2>
+    <form>
+        Store some stuff: 
+        <input type="text" name="stuff">
+        <input type="submit">
+    </form>
+    <table>
+    {% for item in items %}
+    <tr>
+        <td>{{ item['data'] }}</td>
+    </tr>
+    {% endfor %}
+    </table>
+</body>
+</html>
+'''
+
+@app.route('/')
+def hello():
+    data = request.args.get('stuff')
+    if data:
+        mongo.db.stuff.insert_one({'data': data})
+
+    items = mongo.db.stuff.find()
+    return render_template_string(HTML, items=items)
+
+app.run(host='0.0.0.0', port=80)
